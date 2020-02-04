866039fb17cbf9046d0f40abc5188813fa996fa5
Ryan Villanueva <ryanvill@gmail.com>
2020-02-03 23:57:27 -0500

    Refactored and cleaned

--- radio.py
+++ radio.py
@@ -1,127 +1,119 @@
 import pyaudio
-import wave
-import sys
 import keyboard
 from pydub import AudioSegment
 from pydub.playback import play
 from pydub.utils import make_chunks
-from pydub.effects import high_pass_filter
-from pydub.scipy_effects import low_pass_filter
-from pydub.scipy_effects import high_pass_filter
-from pydub.scipy_effects import band_pass_filter
 import time
+from utils import cheap_eq, list_devices
 
-CHUNK = 2048
+enable_playback = True
+
+CHUNK = 1024 * 2
 CHANNELS = 2
 RATE = 44100
 FORMAT = pyaudio.paInt16
 TALK_KEY = '`'
 
-last_cutoff = 0
-talk_is_blocked = False
+last_talk_release_time = 0
+talk_release_is_pending = False
+start_time = time.time()
+burst_is_enabled = True
 
-def cheap_eq(seg,focus_freq,bandwidth=100,mode="peak",gain_dB=0,order=5):
-	'''
-	Cheap EQ in PyDub
-	Silence=-120dBFS
-	I2/I1=2=>3dB SPL Gain
-	'''
-	if gain_dB>=0:
-		if mode=="peak":
-			sec=band_pass_filter(seg,focus_freq-bandwidth/2,focus_freq+bandwidth/2,order=order)
-			pass
-		if mode=="low_shelf":
-			sec=low_pass_filter(seg,focus_freq,order=order)
-			pass
-		if mode=="high_shelf":
-			sec=high_pass_filter(seg,focus_freq,order=order)
-			pass
-		seg=seg.overlay(sec-(3-gain_dB))
-		pass
-	if gain_dB<0:
-		if mode=="peak":
-			sec=band_pass_filter(seg,focus_freq-bandwidth/2,focus_freq+bandwidth/2,order=order)
-			pass
-		if mode=="low_shelf":
-			sec=low_pass_filter(seg,focus_freq,order=order)
-			pass
-		if mode=="high_shelf":
-			sec=high_pass_filter(seg,focus_freq,order=order)
-			pass
-		seg=(seg+gain_dB).overlay(sec-(3+gain_dB))-gain_dB
-		pass
-	return seg
+static_sound = AudioSegment.from_file("./assets/static.wav", format="wav")
+burst_sound = AudioSegment.from_file("./assets/burst.wav", format="wav")
 
 def main():
-    global talk_is_blocked
-    global last_cutoff
+    global talk_release_is_pending
     p = pyaudio.PyAudio()
 
-    staticsound = AudioSegment.from_file("./assets/static.wav", format="wav")
-    endsound = AudioSegment.from_file("./assets/radio-end.wav", format="wav")
+    input_stream = p.open(format=FORMAT,
+        channels=CHANNELS,
+        input=True,
+        rate=RATE,
+        input_device_index=1
+    )
 
-    inputstream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    input=True,
-                    rate=RATE,
-                    input_device_index=1
-                    )
+    primary_output_stream = p.open(format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        output=True,
+        output_device_index=7
+    )
 
-    micoutputstream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    output=True,
-                    output_device_index=7
-                    )
-    playbackoutputstream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    output=True
-                    )
-    elapsed = 0
-    info = p.get_host_api_info_by_index(0)
-    numdevices = info.get('deviceCount')
-    for i in range(0, numdevices):
-        print ("Input Device id " + str(i) + " - " + p.get_device_info_by_host_api_device_index(0, i).get('name'))
+    playback_output_stream = p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        output=True
+    )
 
-
-    def release_talk_callback(keyboard_event):
-        global last_cutoff
-        global talk_is_blocked
-        last_cutoff = keyboard_event.time
-        talk_is_blocked = True
-
-
+    # Ensure keyboard talk release is recorded and delayed until end of burst
     keyboard.on_release_key(TALK_KEY, release_talk_callback, suppress=True)
 
-
     while True:
-        if(talk_is_blocked == True and last_cutoff < (time.time() - endsound.duration_seconds)):
+        talk_is_active = keyboard.is_pressed(TALK_KEY)
+        burst_is_finished = time.time() > (last_talk_release_time + burst_sound.duration_seconds)
+        audio_is_active = talk_is_active or burst_is_finished == False
+
+        keyboard_needs_release = burst_is_enabled and talk_release_is_pending and burst_is_finished
+
+        if audio_is_active:
+            # Get sound from microphone
+            mic_sound = get_mic_sound(input_stream)
+
+            # Add overlays and transformations
+            sound = transform_mic_audio(mic_sound)
+            sound = overlay_static_sound(sound, static_sound)
+            if(burst_is_enabled == True and burst_is_finished == False):
+                sound = overlay_burst_sound(sound, burst_sound)
+
+            # Play
+            if(enable_playback):
+                play(playback_output_stream, sound)
+            play(primary_output_stream, sound)
+
+        elif keyboard_needs_release:
+            # Ensure
             keyboard.release(TALK_KEY)
-            talk_is_blocked = False
+            talk_release_is_pending = False
 
-        if (keyboard.is_pressed(TALK_KEY) or last_cutoff > (time.time() - endsound.duration_seconds)):
-            data = inputstream.read(CHUNK)
-            micsound = AudioSegment(
-                data=data,
-                sample_width=2,
-                frame_rate=44100,
-                channels=2
-            )
-            transformed = cheap_eq(micsound, focus_freq=500, bandwidth=500, mode="peak", gain_dB=15)
-            transformed = transformed.high_pass_filter(1000)
 
-            staticsound_start = elapsed % staticsound.duration_seconds
-            elapsed = elapsed + transformed.duration_seconds
-            staticsound_end = elapsed % staticsound.duration_seconds
+def release_talk_callback(keyboard_event):
+    global last_talk_release_time
+    global talk_release_is_pending
+    last_talk_release_time = time.time()
+    if burst_is_enabled == True:
+        talk_release_is_pending = True
 
-            transformed = (transformed + 15).overlay(staticsound[staticsound_start * 1000: staticsound_end * 1000] - 25)
-            endsound_start = time.time() - last_cutoff
-            endsound_end = endsound_start + transformed.duration_seconds
-            if(endsound.duration_seconds > endsound_start):
-                transformed = transformed.overlay(endsound[endsound_start * 1000: endsound_end * 1000] - 5)
-            micoutputstream.write(transformed._data)
-            playbackoutputstream.write(transformed._data)
+def transform_mic_audio(seg):
+    transformed = cheap_eq(seg, focus_freq=500, bandwidth=500, mode="peak", gain_dB=15)
+    transformed = transformed.high_pass_filter(1000)
+    return transformed
+
+def overlay_static_sound(seg, static):
+    elapsed = start_time - time.time()
+    start = elapsed % static.duration_seconds
+    end = (elapsed + seg.duration_seconds) % static.duration_seconds + seg.duration_seconds
+    transformed = (seg + 15).overlay(static[start * 1000: end * 1000] - 25)
+    return transformed
+
+def overlay_burst_sound(seg, burst):
+    start = time.time() - last_talk_release_time
+    end = start + seg.duration_seconds
+    transformed = seg.overlay(burst[start * 1000: end * 1000] - 5)
+    return transformed
+
+def play(stream, sound):
+    stream.write(sound._data)
+
+def get_mic_sound(input_stream):
+    data = input_stream.read(CHUNK)
+    return AudioSegment(
+        data=data,
+        sample_width=2,
+        frame_rate=44100,
+        channels=2
+    )
 
 
 main()
